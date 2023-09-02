d4c5cb21530e2266504f84fed8214a260f3c75a2
Tarik Bhateja <77834838+Tarik-Bhateja@users.noreply.github.com>
2023-09-02 22:48:30 +0530

    Create reversevowels.cpp

---
+++ reversevowels.cpp
@@ -0,0 +1,18 @@
+ vector<char> v;
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'
+              || s[i] == 'A' || s[i] == 'E' || s[i] == 'O' || s[i] == 'I' || s[i] == 'U')
+                v.push_back(s[i]);
+        }
+        reverse(v.begin(), v.end());
+        int rev = 0;
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'
+              || s[i] == 'A' || s[i] == 'E' || s[i] == 'O' || s[i] == 'I' || s[i] == 'U'){
+                s[i] = v[rev];
+                rev++;
+            }
+        }
+        return s;
