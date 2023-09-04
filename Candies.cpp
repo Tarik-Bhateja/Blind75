3b5b17874567ae1dcbc802b66bbb03b2761091bf
Tarik Bhateja <77834838+Tarik-Bhateja@users.noreply.github.com>
2023-09-04 22:40:28 +0530

    Create Candies.cpp

---
+++ Candies.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+         vector<bool> ans;
+         
+         int max=*(max_element(candies.begin(),candies.end()));
+         
+         for(int i=0;i<candies.size();i++)
+         {
+             candies[i]+=extraCandies;
+         }
+
+         for(int i=0;i<candies.size();i++)
+         {
+            if(candies[i]>=max)
+            {
+                ans.push_back(true);
+            }
+            else
+            {
+                ans.push_back(false);
+            }
+         }
+         return ans;
+    }
+};
